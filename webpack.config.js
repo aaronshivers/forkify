fb52a458f4900b0906f709961a7fdc917853da71
Aaron Shivers <aaron@moonmail.org>
2019-01-26 08:12:18 -0600

    Added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 }
